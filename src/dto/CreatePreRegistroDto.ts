e2017da4fa6380cb29acc1fd771705466f5f20cf
gruxdeveloper1 <gruxdeveloper1@gmail.com>
2024-11-15 13:46:56 -0400

    documentacion

--- src/dto/CreatePreRegistroDto.ts
+++ src/dto/CreatePreRegistroDto.ts
@@ -1,30 +1,39 @@
 // src/pre-registro/dto/pre-registro.dto.ts
 import { IsString, IsArray, IsInt, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreatePreRegistroDto {
+  @ApiProperty()
   @IsString()
   nombres: string;
 
+  @ApiProperty()
   @IsString()
   apellidos: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   telefono: string;
 
+  @ApiProperty()
   @IsString()
   empresa: string;
 
+  @ApiProperty()
   @IsString()
   estatus_correo: string;
 
+  @ApiProperty()
   @IsString()
   estatus: string;
 
   // Campo para las categorías seleccionadas
+  @ApiProperty()
   @IsArray()
-  @IsInt({ each: true })  // Validamos que cada elemento del array sea un número entero
-  categorias: number[];    // Lista de IDs de las categorías seleccionadas
+  @IsInt({ each: true }) // Validamos que cada elemento del array sea un número entero
+  categorias: number[]; // Lista de IDs de las categorías seleccionadas
 }
--- src/preRegistro/pre-registro.controller.ts
+++ src/preRegistro/pre-registro.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get, Post, Body } from '@nestjs/common';
 import { PreRegistroService } from './pre-registro.service';
 import { PreRegistro } from '../entities/pre-registro.entity';
+import { ApiResponse } from '@nestjs/swagger';
+import { CreatePreRegistroDto } from 'src/dto/CreatePreRegistroDto';
 
 @Controller('pre-registro')
 export class PreRegistroController {
   constructor(private readonly preRegistroService: PreRegistroService) {}
 
   @Post()
+  @ApiResponse({
+    status: 201,
+    description: 'The record has been successfully created.',
+    type: CreatePreRegistroDto,
+  })
   async create(@Body() preRegistroData: Partial<PreRegistro>) {
     return this.preRegistroService.create(preRegistroData);
   }
